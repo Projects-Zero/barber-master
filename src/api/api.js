c6dc1e4bcac795dc284e243a6caa1a341348f5a9
alanhcrdz <alanhcrdz@gmail.com>
2023-03-16 08:16:54 -0300

    set up env

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const BASE_URL = process.env.NODE_ENV == 'development' ? 
-'http://192.168.100.66:3500' : 
-'barber-master-backend.vercel.app';
+process.env.IP_ADDRESS : 
+process.env.BACKEND_URL;
 
 
 
